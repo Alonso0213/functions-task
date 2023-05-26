e80cde49fbd1818684cef239fa05b61c8190a81f
Alonso0213 <alonsocupido51@gmail.com>
2023-05-26 14:05:34 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,17 @@
+function greet(para1 = "hello"){
+console.log(para1);
+}
+greet()
+function multiply(num1, num2){
+    num1= 5;
+    num2= 5;
+    let result = num1 * num2
+    console.log(result);
+}
+multiply()
+
+function tableofFive(me){
+    return me *5
+
+}
+console.log(tableofFive(2));
